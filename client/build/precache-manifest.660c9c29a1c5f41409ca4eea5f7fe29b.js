585aa80e9c8f213f7aedf2b89a33cf0a7257f3fb
eman <eman_h_2009@live.com>
2019-05-14 21:47:35 +0300

    init

---
+++ client/build/precache-manifest.660c9c29a1c5f41409ca4eea5f7fe29b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ebfba426169206ea57a2c69c06f1b24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c421e5db5b17e39fa5e",
+    "url": "/static/css/main.45d51e7f.chunk.css"
+  },
+  {
+    "revision": "c3e7d62f839773d5e2f3",
+    "url": "/static/js/2.2ec53817.chunk.js"
+  },
+  {
+    "revision": "9c421e5db5b17e39fa5e",
+    "url": "/static/js/main.a3ebc2e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
