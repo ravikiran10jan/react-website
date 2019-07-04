642ffa8b6c9b664a7f4b337103a2e89c462e76cf
eman <eman_h_2009@live.com>
2019-07-04 13:05:58 +0300

    style

--- client/src/Components/profile/ImageUplode.jsx
+++ client/src/Components/profile/ImageUplode.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const ImageUpload = ({label,progress,handleChangeImage,handleUploadImage}) => {
-  return (
-    <div className="input-group">
-      <label className="label">{label}</label>
-      <progress value={progress} max="100" />
-      <br />
-      <input className="input--style-4" type="file" onChange={handleChangeImage} />
-      <button onClick={handleUploadImage}>Upload</button>
-      <br />
-    </div>
-  );
-};
+const ImageUpload = ({
+  label, progress, handleChangeImage, handleUploadImage,
+}) => (
+  <div className="input-group">
+    <label className="label">{label}</label>
+    <progress value={progress} max="100" />
+    <br />
+    <input className="input--style-4" type="file" onChange={handleChangeImage} />
+    {/* <button className="btn-uplodeImg " onClick={handleUploadImage}>Upload</button> */}
+    <br />
+  </div>
+);
 
 
 export default ImageUpload;
--- client/src/Components/profile/input.jsx
+++ client/src/Components/profile/input.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-const Input = ({label,name,type,onChange,value
-}) => {
-  return (
-    <div className="input-group">
-      <label className="label">{label}</label>
-      <input className="input--style-4" type={type} name={name} value={value} onChange={onChange} />
-    </div>
-  );
-};
+const Input = ({
+  label, name, type, onChange, value,
+}) => (
+  <div className="input-group">
+    <label className="label">{label}</label>
+    <input className="input--style-4 " type={type} name={name} value={value} onChange={onChange} />
+  </div>
+);
 
 
 export default Input;
